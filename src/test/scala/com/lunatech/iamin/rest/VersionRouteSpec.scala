458877df54b3a9ec6f7bf5bd67df883811ce9b81
Georgy Chikhladze <georgy.chikhladze@lunatech.nl>
2019-04-16 17:55:19 +0200

    Get rid of "Hello world" naming

--- src/test/scala/com/lunatech/iamin/rest/VersionRouteSpec.scala
+++ src/test/scala/com/lunatech/iamin/rest/VersionRouteSpec.scala
@@ -23,9 +23,9 @@ class VersionRouteSpec extends org.specs2.mutable.Specification {
   }
 
   private[this] val retVersion: Response[IO] = {
-    val getHW = Request[IO](Method.GET, Uri.uri("/version"))
+    val getVersion = Request[IO](Method.GET, Uri.uri("/version"))
     val version = VersionHandler.impl[IO](BuildInfo)
-    new VersionResource[IO]().routes(version).orNotFound(getHW).unsafeRunSync()
+    new VersionResource[IO]().routes(version).orNotFound(getVersion).unsafeRunSync()
   }
 
   private[this] def uriReturns200(): MatchResult[Status] =

7ba8b8507db565f24146f1ee92fad5c7edf2c590
Adrien Haxaire <adrien.haxaire@lunatech.nl>
2019-09-04 11:46:40 +0200

    Some working text display

--- frontend/src/main/scala/com/lunatech/iamin/App.scala
+++ frontend/src/main/scala/com/lunatech/iamin/App.scala
@@ -2,22 +2,7 @@ import scala.scalajs.js
 import org.scalajs.dom
 import japgolly.scalajs.react._
 import japgolly.scalajs.react.vdom.html_<^._
-
-case class AppState(user: String)
-
-object IaminApp {
-
-  val component = ScalaComponent.builder[Unit]("Iamin")
-    .initialState(AppState("toto"))
-    .renderBackend[AppBackend]
-    .build
-
-  class AppBackend($: BackendScope[Unit, AppState]) {
-
-    def render(s: AppState) = <.div(<.p(s.user))
-
-  }
-}
+import com.lunatech.iamin.IaminApp
 
 object App extends js.JSApp {
 
--- frontend/src/main/scala/com/lunatech/iamin/IaminApp.scala
+++ frontend/src/main/scala/com/lunatech/iamin/IaminApp.scala
@@ -3,6 +3,19 @@ package com.lunatech.iamin
 import japgolly.scalajs.react._
 import japgolly.scalajs.react.vdom.html_<^._
 
-import scala.concurrent.Future
-import scala.concurrent.ExecutionContext.Implicits.global
+object IaminApp {
 
+  case class AppState(user: String)
+
+  val component = ScalaComponent.builder[Unit]("Iamin")
+    .initialState(AppState("toto"))
+    .renderBackend[AppBackend]
+    .build
+
+  class AppBackend($: BackendScope[Unit, AppState]) {
+
+//    val userState = $.zoom(_.user)((s, x) => s.copy(user = x))
+
+    def render(s: AppState) = <.div(<.p(s.user))
+  }
+}

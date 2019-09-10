cdcb44245b91670489dd6fa4c548438dee77370d
genegr88 <evgenii.frank@lunatech.nl>
2019-09-10 15:17:42 +0200

    Declare right namespace for the App

--- frontend/src/main/scala/com/lunatech/iamin/App.scala
+++ frontend/src/main/scala/com/lunatech/iamin/App.scala
@@ -1,8 +1,9 @@
+package com.lunatech.iamin
+
 import scala.scalajs.js
 import org.scalajs.dom
 import japgolly.scalajs.react._
 import japgolly.scalajs.react.vdom.html_<^._
-import com.lunatech.iamin.IaminApp
 
 object App extends js.JSApp {
 
